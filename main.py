9dff398adf63a66e24ba1e70c872831eb171bf8c
sdfgsdsdgdgds-lgtm <sdfgsdsdgdgds@gmail.com>
2025-10-16 23:03:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-import os
-import asyncio
 import discord
+import asyncio
 
-TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-MESSAGE_TEXT = os.getenv("MESSAGE_TEXT", "@everyone Test!")
-SPAM_DELAY = float(os.getenv("SPAM_DELAY", "1"))
+TOKEN = "din_token_här"
+CHANNEL_ID = 1234567890  # byt ut mot riktig kanal-ID
+SPAM_MESSAGE = "Detta är ett spammeddelande"
+SPAM_DELAY = 1  # sekunder
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.messages = True
 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    print(f"Inloggad som {client.user}")
     channel = client.get_channel(CHANNEL_ID)
-    if channel is None:
-        print("Can't find channel")
-        return
     while True:
-        try:
-            await channel.send(MESSAGE_TEXT)
-            await asyncio.sleep(SPAM_DELAY)
-        except Exception as e:
-            print("Error while sending:", e)
-            await asyncio.sleep(5)
+        await channel.send(SPAM_MESSAGE)
+        await asyncio.sleep(SPAM_DELAY)
 
 client.run(TOKEN)
-
 
 
